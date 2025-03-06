d455f458f6d241d8f1019c11f73c2d1ccacd2932
sndp bag <sndpbagg@gmail.com>
2025-03-06 17:23:45 +0530

    chate left side section copleted

--- src/Components/ChatArea.jsx
+++ src/Components/ChatArea.jsx
@@ -26,6 +26,145 @@ const ChatArea = () => {
             </div>
             {/* end navbar */}
 
+            {/* message area */}
+            <div className='message-area w-full h-[400px] border-[1px] px-10 border-slate-200 overflow-auto'>
+
+ <div className="chat chat-start">
+  <div className="chat-image avatar">
+    <div className="w-10 rounded-full">
+      <img
+        alt="Tailwind CSS chat bubble component"
+        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+    </div>
+  </div>
+  <div className="chat-header">
+    Obi-Wan Kenobi
+    <time className="text-xs opacity-50">12:45</time>
+  </div>
+
+  <div className="chat-bubble">You were the Chosen One!</div>
+  <div className="chat-footer opacity-50">Delivered</div>
+</div>
+
+
+
+
+
+<div className="chat chat-end">
+  <div className="chat-image avatar">
+    <div className="w-10 rounded-full">
+      <img
+        alt="Tailwind CSS chat bubble component"
+        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+    </div>
+  </div>
+  <div className="chat-header">
+    Anakin
+    <time className="text-xs opacity-50">12:46</time>
+  </div>
+  <div className="chat-bubble">I hate you!</div>
+  <div className="chat-footer opacity-50">Seen at 12:46</div>
+</div>
+
+
+<div className="chat chat-start">
+  <div className="chat-image avatar">
+    <div className="w-10 rounded-full">
+      <img
+        alt="Tailwind CSS chat bubble component"
+        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+    </div>
+  </div>
+  <div className="chat-header">
+    Obi-Wan Kenobi
+    <time className="text-xs opacity-50">12:45</time>
+  </div>
+
+  <div className="chat-bubble">You were the Chosen One!</div>
+  <div className="chat-footer opacity-50">Delivered</div>
+</div>
+
+
+
+<div className="chat chat-end">
+  <div className="chat-image avatar">
+    <div className="w-10 rounded-full">
+      <img
+        alt="Tailwind CSS chat bubble component"
+        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+    </div>
+  </div>
+  <div className="chat-header">
+    Anakin
+    <time className="text-xs opacity-50">12:46</time>
+  </div>
+  <div className="chat-bubble">I hate you!</div>
+  <div className="chat-footer opacity-50">Seen at 12:46</div>
+</div>
+
+
+<div className="chat chat-start">
+  <div className="chat-image avatar">
+    <div className="w-10 rounded-full">
+      <img
+        alt="Tailwind CSS chat bubble component"
+        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+    </div>
+  </div>
+  <div className="chat-header">
+    Obi-Wan Kenobi
+    <time className="text-xs opacity-50">12:45</time>
+  </div>
+
+  <div className="chat-bubble">You were the Chosen One!</div>
+  <div className="chat-footer opacity-50">Delivered</div>
+</div>
+
+
+
+<div className="chat chat-end">
+  <div className="chat-image avatar">
+    <div className="w-10 rounded-full">
+      <img
+        alt="Tailwind CSS chat bubble component"
+        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+    </div>
+  </div>
+  <div className="chat-header">
+    Anakin
+    <time className="text-xs opacity-50">12:46</time>
+  </div>
+  <div className="chat-bubble">I hate you!</div>
+  <div className="chat-footer opacity-50">Seen at 12:46</div>
+</div>
+
+
+<div className="chat chat-start">
+  <div className="chat-image avatar">
+    <div className="w-10 rounded-full">
+      <img
+        alt="Tailwind CSS chat bubble component"
+        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+    </div>
+  </div>
+  <div className="chat-header">
+    Obi-Wan Kenobi
+    <time className="text-xs opacity-50">12:45</time>
+  </div>
+
+  <div className="chat-bubble">You were the Chosen One!</div>
+  <div className="chat-footer opacity-50">Delivered</div>
+</div>
+
+            </div>
+
+            {/* end message area */}
+
+                <div className="chat-input w-full h-[125px] border border-slate-200">
+
+                </div>
+
+
 
         </div>
     );
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { ChartArea, EllipsisVertical, Menu, MessageCircle, Phone, Search, UserRound } from 'lucide-react';
+import { ChartArea, Dot, EllipsisVertical, Menu, MessageCircle, Phone, Search, UserRound, Video } from 'lucide-react';
 import React from 'react';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
@@ -24,7 +24,7 @@ const Sidebar = () => {
 
             <Tabs className='  border-0'>
     <TabList className=' flex justify-between items-center   px-5 py-5'>
-      <Tab className='focus:outline-none px-2 py-3 text-slate-500 font-extrabold cursor-pointer flex gap-2'> <MessageCircle /> Chats</Tab>
+      <Tab className='focus:outline-none px-2 py-3 text-slate-500 font-extrabold cursor-pointer  flex gap-2'> <MessageCircle /> Chats</Tab>
       <Tab className='focus:outline-none px-2 py-3  text-slate-500 font-extrabold cursor-pointer flex gap-2'><Phone /> Calls</Tab>
       <Tab className='focus:outline-none px-2 py-3 text-slate-500 font-extrabold cursor-pointer flex gap-2'> <UserRound /> Contacts</Tab>
     </TabList>
@@ -35,13 +35,175 @@ const Sidebar = () => {
     </div>
 
     <TabPanel>
-      <h2>Any content 1</h2>
+      <h2 className='text-sm font-bold text-slate-400 ml-5 uppercase'>Recent Chats</h2>
+      {/*  this is chart Section */}
+      <div className="chats overflow-auto custom-scrollbar h-[360px]" style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}>
+        {/* single chart section */}
+        <div className="single_chat bg-violet-100 cursor-pointer hover:bg-violet-300  py-4 flex justify-between items-center px-2">
+         <div className='flex gap-2'>
+         <img src="../../public/my image.jpg" className='w-11 h-11 rounded-full' alt="" />
+          <div className="name space-y--1">
+              <h2 className='uppercase text-sm'>Sandipan kr bag</h2>
+              <span className='message text-[13px]'>How are you</span>
+          </div>
+         </div>
+
+          <div className='flex flex-col space-y-2'>
+            <span>10:35 pm</span>
+            <span className='w-5 h-5 rounded-full bg-violet-600 text-sm  text-white flex ml-10 justify-center items-center '>2</span>
+          </div>
+        </div>
+
+        <div className="single_chat bg-violet-100 cursor-pointer hover:bg-violet-300  py-4 flex justify-between items-center px-2">
+         <div className='flex gap-2'>
+         <img src="../../public/my image.jpg" className='w-11 h-11 rounded-full' alt="" />
+          <div className="name space-y--1">
+              <h2 className='uppercase text-sm'>Sandipan kr bag</h2>
+              <span className='message text-[13px]'>How are you</span>
+          </div>
+         </div>
+
+          <div className='flex flex-col space-y-2'>
+            <span>10:35 pm</span>
+            <span className='w-5 h-5 rounded-full bg-violet-600 text-sm  text-white flex ml-10 justify-center items-center '>2</span>
+          </div>
+        </div>
+
+
+
+        <div className="single_chat bg-violet-100 cursor-pointer hover:bg-violet-300  py-4 flex justify-between items-center px-2">
+         <div className='flex gap-2'>
+         <img src="../../public/my image.jpg" className='w-11 h-11 rounded-full' alt="" />
+          <div className="name space-y--1">
+              <h2 className='uppercase text-sm'>Sandipan kr bag</h2>
+              <span className='message text-[13px]'>How are you</span>
+          </div>
+         </div>
+
+          <div className='flex flex-col space-y-2'>
+            <span>10:35 pm</span>
+            <span className='w-5 h-5 rounded-full bg-violet-600 text-sm  text-white flex ml-10 justify-center items-center '>2</span>
+          </div>
+        </div>
+
+
+        <div className="single_chat bg-violet-100 cursor-pointer hover:bg-violet-300  py-4 flex justify-between items-center px-2">
+         <div className='flex gap-2'>
+         <img src="../../public/my image.jpg" className='w-11 h-11 rounded-full' alt="" />
+          <div className="name space-y--1">
+              <h2 className='uppercase text-sm'>Sandipan kr bag</h2>
+              <span className='message text-[13px]'>How are you</span>
+          </div>
+         </div>
+
+          <div className='flex flex-col space-y-2'>
+            <span>10:35 pm</span>
+            <span className='w-5 h-5 rounded-full bg-violet-600 text-sm  text-white flex ml-10 justify-center items-center '>2</span>
+          </div>
+        </div>
+
+        <div className="single_chat bg-violet-100 cursor-pointer hover:bg-violet-300  py-4 flex justify-between items-center px-2">
+         <div className='flex gap-2'>
+         <img src="../../public/my image.jpg" className='w-11 h-11 rounded-full' alt="" />
+          <div className="name space-y--1">
+              <h2 className='uppercase text-sm'>Sandipan kr bag</h2>
+              <span className='message text-[13px]'>How are you</span>
+          </div>
+         </div>
+
+          <div className='flex flex-col space-y-2'>
+            <span>10:35 pm</span>
+            <span className='w-5 h-5 rounded-full bg-violet-600 text-sm  text-white flex ml-10 justify-center items-center '>2</span>
+          </div>
+        </div>
+
+        <div className="single_chat bg-violet-100 cursor-pointer hover:bg-violet-300  py-4 flex justify-between items-center px-2">
+         <div className='flex gap-2'>
+         <img src="../../public/my image.jpg" className='w-11 h-11 rounded-full' alt="" />
+          <div className="name space-y--1">
+              <h2 className='uppercase text-sm'>Sandipan kr bag</h2>
+              <span className='message text-[13px]'>How are you</span>
+          </div>
+         </div>
+
+          <div className='flex flex-col space-y-2'>
+            <span>10:35 pm</span>
+            <span className='w-5 h-5 rounded-full bg-violet-600 text-sm  text-white flex ml-10 justify-center items-center '>2</span>
+          </div>
+        </div>
+
+        <div className="single_chat bg-violet-100 cursor-pointer hover:bg-violet-300  py-4 flex justify-between items-center px-2">
+         <div className='flex gap-2'>
+         <img src="../../public/my image.jpg" className='w-11 h-11 rounded-full' alt="" />
+          <div className="name space-y--1">
+              <h2 className='uppercase text-sm'>Sandipan kr bag</h2>
+              <span className='message text-[13px]'>How are you</span>
+          </div>
+         </div>
+
+          <div className='flex flex-col space-y-2'>
+            <span>10:35 pm</span>
+            <span className='w-5 h-5 rounded-full bg-violet-600 text-sm  text-white flex ml-10 justify-center items-center '>2</span>
+          </div>
+        </div>
+        {/*  end single chart */}
+      </div>
     </TabPanel>
     <TabPanel>
-      <h2>Any content 2</h2>
+      {/*  for calls  */}
+       
+      {/*  this is chart Section */}
+      <div className="chats overflow-auto flex  justify-center h-[360px] mt-2" style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}>
+      
+      
+      
+        {/* single call section  1*/}
+        <div className="single_chat w-[90%] h-[70px] cursor-pointer shadow-sm mt-2 rounded-2xl  py-4 flex justify-between items-center px-2">
+         <div className='flex gap-2'>
+         <img src="../../public/my image.jpg" className='w-11 h-11 rounded-full' alt="" />
+          <div className="name space-y--1">
+              <h2 className='uppercase text-sm'>Sandipan kr bag</h2>
+              <div className='flex items-center gap-2'>
+                
+              <span className='message text-[13px] flex gap-1  items-center'> <Video className='text-green-400 fill-current' /> Outgoing  </span> <span>. Today, 2:30 PM</span>
+              </div>
+          </div>
+         </div>
+
+          <button className='w-10 h-10 bg-violet-100 flex justify-center items-center rounded-full'>
+          <Phone className='text-violet-500 w-5 h-5' />
+          </button>
+        </div>
+
+
+ 
+
+
+
+
+        </div>
+      
     </TabPanel>
     <TabPanel>
-      <h2>Any content 3</h2>
+    <h2 className='text-sm font-bold text-slate-400 ml-5 uppercase'>Recent Chats</h2>
+      {/*  this is all Contact Section */}
+      <div className="chats overflow-auto custom-scrollbar h-[360px]" style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}>
+        {/* single contact section */}
+        <div className="single_chat bg-violet-100 cursor-pointer hover:bg-violet-300  py-4 flex justify-between items-center px-2">
+         <div className='flex gap-2'>
+         <img src="../../public/my image.jpg" className='w-11 h-11 rounded-full' alt="" />
+          <div className="name space-y--1">
+              <h2 className='uppercase text-sm'>Sandipan kr bag</h2>
+              <span className='message text-[13px] flex items-center'><span className='w-6 h-6  rounded-full text-green-600 flex items-center'><Dot className='w-10 h-10' /></span> Online</span>
+          </div>
+         </div>
+
+          <div className='flex gap-2 items-start space-y-2'>
+           <span><Video className='text-slate-500 hover:text-green-500 fill-current'></Video></span>
+           <span><Phone className='text-slate-500 hover:text-green-500 fill-current'></Phone></span>
+          </div>
+        </div>
+        </div>
     </TabPanel>
   </Tabs>
 
