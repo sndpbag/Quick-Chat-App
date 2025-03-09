b10a806e3a0714f9d4bfd6b5810931a14a482904
sndp bag <sndpbagg@gmail.com>
2025-03-09 20:44:45 +0530

    fix: allowedHosts issue fixed in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,47 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+
+//   ],
+//   server: {
+//     host: true,
+//     port: 3000, // বা তোমার পোর্ট নম্বর
+//     strictPort: true,
+//     hmr: {
+//       clientPort: 443, // Render সার্ভারে HMR কাজ করাতে
+//     },
+//     preview: {
+//       allowedHosts: ["quick-chat-app-i130.onrender.com"] // এখানেই হোস্ট অনুমোদিত করতে হবে
+//     }
+//   }
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
+  plugins: [react(),
     tailwindcss(),
-
   ],
   server: {
     host: true,
-    port: 3000, // বা তোমার পোর্ট নম্বর
+    port: 3000, // বা তোমার পছন্দের পোর্ট নম্বর
     strictPort: true,
     hmr: {
       clientPort: 443, // Render সার্ভারে HMR কাজ করাতে
     },
-    preview: {
-      allowedHosts: ["quick-chat-app-i130.onrender.com"] // এখানেই হোস্ট অনুমোদিত করতে হবে
-    }
+  },
+  preview: {
+    allowedHosts: ["quick-chat-app-i130.onrender.com"] // এখানেই হোস্ট অনুমোদিত করতে হবে
   }
 })
+
